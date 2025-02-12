ab160f20bfc18d94904b27a85dcb0e6f763f7676
Dmitry <tilin@applicatura.com>
2025-02-12 21:22:43 +0700

    Ex5: Парсинг JSON

---
+++ src/test/java/Homework/getJSONHW.java
@@ -0,0 +1,24 @@
+package Practice;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+public class getJSONHW {
+
+  @Test
+  public void getJSONHW(){
+    JsonPath response = (JsonPath) RestAssured
+
+            .get("https://playground.learnqa.ru/api/get_json_homework")
+            .jsonPath();
+
+    System.out.println("Текст второго сообщения: " + response.get("messages[1].message"));
+
+    /* Без отдельного параметра код короче
+    String message = response.get("messages[1].message");
+    System.out.println("Текст второго сообщения: " + message);*/
+
+  }
+}
+

8d9dbd856b6a71f360300daf1e1e963e461b1144
Dmitry <tilin@applicatura.com>
2025-02-13 16:56:26 +0700

    Ex6: Редирект

---
+++ src/test/java/Homework/TestRedirectHW.java
@@ -0,0 +1,22 @@
+package Homework;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class TestRedirectHW {
+
+  @Test
+  public void testRedirectHW(){
+
+    Response response = RestAssured
+            .given()
+            .redirects()
+            .follow(false)
+            .when()
+            .get("https://playground.learnqa.ru/api/long_redirect")
+            .andReturn();
+    response.prettyPrint();
+    System.out.println("Redirect Location is: " + response.getHeader("Location"));
+  }
+}
